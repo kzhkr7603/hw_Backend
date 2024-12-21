50b443a6edc7c235ff13929b94a1b8e5ae7fa165
kzhkr7603 <112762552+kzhkr7603@users.noreply.github.com>
2024-12-22 05:48:47 +0900

    Update TrainingController.kt

--- src/main/kotlin/kr/ac/kumoh/s20210296/hw_Backend/controller/TrainingController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210296/hw_Backend/controller/TrainingController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/Training")
-@CrossOrigin(origins = ["http://localhost:3001"])
+@CrossOrigin(origins = ["http://localhost:3001/"])
 class TrainingController(private val service: TrainingService) {
     @PostMapping
     fun addTraining(@RequestBody training: Training): Training = service.addTraining(training)
